b826171e10f205392fa687f2958b4537330ffb58
Kakadu <Kakadu@pm.me>
2024-09-08 11:07:12 +0300

    PPX_repr was understood. PPX_fresh still behaves weird
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- lib/ppx_repr.ml
+++ lib/ppx_repr.ml
@@ -3,8 +3,7 @@ open Ppxlib
 let string_of_expression e =
   Format.set_margin 1000;
   Format.set_max_indent 2000;
-  let ans = Format.asprintf "%a" Pprintast.expression e in
-  ans
+  Format.asprintf "%a" Pprintast.expression e
 
 let mapper =
   object
@@ -15,13 +14,18 @@ let mapper =
       | { pexp_desc = Pexp_construct ({ txt = Lident "REPR"; _ }, Some e); _ }
         ->
           let text = string_of_expression e in
+          let pexp_loc = { e.pexp_loc with loc_ghost = true } in
           {
             e with
+            pexp_loc;
             pexp_desc =
               Pexp_tuple
                 [
-                  Ast_helper.Exp.constant ~loc:e.pexp_loc
-                    (Pconst_string (text, e.pexp_loc, None));
+                  Ast_helper.Exp.constant (* ~loc:Location.none *)
+                    ~loc:pexp_loc
+                    (Pconst_string (text, pexp_loc, None));
+                  (* (let loc = pexp_loc in
+                     [%expr 1 ^ ""]); *)
                   e;
                 ];
           }
